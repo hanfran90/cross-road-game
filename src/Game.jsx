5d8aa99330d3c1f58061e89387f3540ed5be0537
Hannah Reed <hannahfrancesreed@gmail.com>
2025-02-27 11:20:14 +0000

    game setup

---
+++ src/Game.jsx
@@ -0,0 +1,10 @@
+import { Scene } from "./components/Scene.jsx";
+import { Player } from "./components/Player.jsx";
+
+export default function Game() {
+  return (
+    <Scene>
+      <Player />
+    </Scene>
+  );
+}
